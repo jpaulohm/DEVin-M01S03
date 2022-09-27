a20f526dcccec1149c429feaa10c06dc0dc89ab7
Joao P Manfredini <jpaulohm@gmail.com>
2022-09-27 15:25:58 -0300

    Adicionado construtor, toStrig(), Promover(), Aumentar()

---
+++ src/Exercicios/Funcionario.java
@@ -0,0 +1,34 @@
+package Exercicios;
+
+//Crie uma classe 'Funcionario' para representar um empregado de uma empresa, contendo os atributos cpf, nome completo e salário.
+//Implemente também um método 'promover' que recebe como parâmetro um percentual (float), devendo o salário ser aumentado pelo percentual indicado no argumento do método.
+//Crie e utilize um objeto desta classe para testar a implementação.
+
+public class Funcionario {
+    Long cpf;
+    String nomeCompleto;
+    Float salario;
+    public Funcionario(Float Salario, String NomeCompleto, Long CPF){
+        cpf=CPF;
+        nomeCompleto=NomeCompleto;
+        salario=Salario;
+
+    }
+    public Float Promover(Float percentual){
+      this.salario = this.salario * (1 + (percentual/100));
+      return this.salario;
+    }
+
+    public Float Aumentar(Float valor, Float comissao){
+        this.salario = this.salario + valor + comissao;
+        return this.salario;
+    }
+    public Float Aumentar(Float valor){
+        this.salario = this.salario + valor;
+        return this.salario;
+    }
+    @Override
+    public String toString(){
+    return "Nome: " + nomeCompleto +"\nCPF: " + cpf + "\nSalario: " + salario;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import Exercicios.Funcionario;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("1)");
+        Funcionario joao = new Funcionario(1000.10F, "Joao Paulo Harger", 5582677933L );
+        System.out.println(joao);
+
+        joao.Promover(10.5F);
+        System.out.println(joao);
+
+        joao.Aumentar(15F);
+        System.out.println(joao);
+
+        joao.Aumentar(15F+20F);
+        System.out.println(joao);
+
     }
 }
